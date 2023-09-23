6e588b321f4e1fbf766eddc81a5be35300ec3769
PedroBicalhoF <142627469+PedroBicalhoF@users.noreply.github.com>
2023-09-23 16:44:00 -0300

    Update script.js

--- criptografia/script.js
+++ criptografia/script.js
@@ -2,10 +2,7 @@ texto = document.getElementById('entrada');
 saida = document.getElementById('resultado');
 bt_2 = document.getElementById('bt2');
 area = document.getElementsByClassName('alfabeto')[0];
-dicionario = document.getElementById('abc');
-chave = document.getElementById('cripto');
-ch = document.getElementById('chave');
-revelado = false;
+chave = document.getElementsByTagName('figure')[0];
 
 const abc = ['0','1','2','3','4','5','6','7','8','9',
 'A','B','C','D','E','F','G','H','I','J','K','L','M',
@@ -16,37 +13,47 @@ function Calcular(){
     const entrada = texto.value.toUpperCase();
     let numero = [];
     
-    
     for(i=0;i<entrada.length;i++){
         let ind = abc.indexOf(entrada[i]);
         numero[i] = cripto[ind];
     }
+    for(i=0;i<36;i++){
+        var alfn = document.createElement('h1');
+        var alfcripto = document.createElement('h2');
+        alfn.innerHTML = abc[i]+': ';
+        chave.appendChild(alfn);
+        alfcripto.innerHTML = cripto[i];
+        chave.appendChild(alfcripto);
+    }
     numero = numero.join('');
     saida.value = numero;
-    chave.innerHTML = cripto;
-    revelado?chave.style.opacity = '1':chave.style.opacity = '0';
 }
 
 function revelar(){
-    revelado = true;
-    chave.style.opacity = '1';
-    dicionario.innerHTML = abc;
-    ch.innerHTML = 'Chave';
     if(texto.type=='text'){
-        revelado = false;
         bt_2.innerHTML = 'Revelar';
         texto.type='password';
-        dicionario.innerHTML='';
-        chave.innerHTML='';
-        ch.innerHTML='';
+        chave.style.opacity = '0';
         return
+    }
+    else{
+        bt_2.innerHTML = 'Esconder';
+        texto.type='text';
+        chave.style.opacity = '1';
     }
     
     window.alert('Nivel de Acesso Exigido')
     senha = prompt('Digite a senha: ')
     
-    senha=='criptografia'? texto.type = 'text':alert('Acesso Negado');
-    bt_2.innerHTML = 'Esconder';
+    if(senha=='criptografia'){
+        revelado = true;
+        texto.type = 'text';
+        bt_2.innerHTML = 'Esconder';
+    }
+    else{
+        alert('Acesso Negado');
+    }
+    
 }
 
 function criptografar(v) {//Durstenfeld shuffle algorithm 
