0331bc3de6bbd27f9682b8c1b24a3cd91f3f3724
PedroBicalhoF <142627469+PedroBicalhoF@users.noreply.github.com>
2023-09-24 15:40:36 -0300

    Update script.js

--- criptografia/script.js
+++ criptografia/script.js
@@ -1,32 +1,43 @@
 texto = document.getElementById('entrada');
 saida = document.getElementById('resultado');
 bt_2 = document.getElementById('bt2');
-area = document.getElementsByClassName('alfabeto')[0];
 chave = document.getElementsByTagName('figure')[0];
+calculado = false;
+alfn = []
+alfcripto = []
+const abc = [];
 
-const abc = ['0','1','2','3','4','5','6','7','8','9',
-'A','B','C','D','E','F','G','H','I','J','K','L','M',
-'N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+for(i=0;i<=94;i++){
+    abc[i] = String.fromCharCode(i+33);
+}
+for(i=95;i<=127;i++){
+    abc[i] = String.fromCharCode(i+66);
+}
+for(i=128;i<221;i++){
+    abc[i] = String.fromCharCode(i+33);
+}
 
 function Calcular(){
+    console.log(abc)
     cripto = criptografar(abc); 
-    const entrada = texto.value.toUpperCase();
+    const entrada = texto.value;
     let numero = [];
     
     for(i=0;i<entrada.length;i++){
         let ind = abc.indexOf(entrada[i]);
         numero[i] = cripto[ind];
     }
-    for(i=0;i<36;i++){
-        var alfn = document.createElement('h1');
-        var alfcripto = document.createElement('h2');
-        alfn.innerHTML = abc[i]+': ';
-        chave.appendChild(alfn);
-        alfcripto.innerHTML = cripto[i];
-        chave.appendChild(alfcripto);
+    if(calculado){
+        remocao();
+        mapear();
+    }
+    else{
+        calculado = true;
+        mapear();
     }
     numero = numero.join('');
     saida.value = numero;
+    console.log(abc[221])
 }
 
 function revelar(){
@@ -37,7 +48,7 @@ function revelar(){
         return
     }
     else{
-        bt_2.innerHTML = 'Esconder';
+        bt_2.innerHTML = 'Ocultar';
         texto.type='text';
         chave.style.opacity = '1';
     }
@@ -46,12 +57,13 @@ function revelar(){
     senha = prompt('Digite a senha: ')
     
     if(senha=='criptografia'){
-        revelado = true;
         texto.type = 'text';
-        bt_2.innerHTML = 'Esconder';
+        bt_2.innerHTML = 'Ocultar';
+        return
     }
     else{
         alert('Acesso Negado');
+        return
     }
     
 }
@@ -66,4 +78,26 @@ function criptografar(v) {//Durstenfeld shuffle algorithm
         vetor[y] = temp;
     }
     return vetor;
+}
+function mapear(){
+    for(i=0;i<abc.length;i++){
+        alfn[i] = document.createElement('h3');
+        alfcripto[i] = document.createElement('h4');
+
+        alfn[i].classList.add('elemento1');
+        alfcripto[i].classList.add('elemento2');
+        
+        alfn[i].innerHTML = abc[i]+' : ';
+        chave.appendChild(alfn[i]);
+        alfcripto[i].innerHTML = cripto[i];
+        chave.appendChild(alfcripto[i]);
+    }
+    return
+}
+function remocao(){
+    for(i=(abc.length-1);i>=0;i--){
+        alfn[i].remove();
+        alfcripto[i].remove();
+    }
+    return
 }
