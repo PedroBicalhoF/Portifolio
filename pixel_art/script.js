d69b00a60717c1a5890690f2800d9dd635959f4f
PedroBicalhoF <142627469+PedroBicalhoF@users.noreply.github.com>
2023-11-23 14:03:35 -0300

    Update script.js

--- pixel_art/script.js
+++ pixel_art/script.js
@@ -1,6 +1,8 @@
 var quadro = document.getElementsByTagName('table')[0];
 var cor = document.getElementsByTagName('input')[0];
-var icone = document.getElementsByTagName('i')[0];
+var icone = document.getElementById('balde');
+var visao = document.getElementById('olho');
+var borracha = document.getElementById('borracha');
 var fundo = cor.value;
 var cor_atual;
 var cor_nova;
@@ -8,6 +10,21 @@ var tamanho = prompt('Digite o tamanho(10-50)');
 
 if(tamanho<10 || tamanho>50){
     tamanho = 10;
+}
+
+visao.onclick = () => {
+    if(visao.className == "fa-regular fa-eye"){
+        quadro.style.visibility = 'hidden';
+        visao.className = "fa-regular fa-eye-slash"; 
+    }
+    else{
+        quadro.style.visibility = 'visible'; 
+        visao.className = "fa-regular fa-eye";
+    }
+}
+
+borracha.onclick = () =>{
+    fundo = "#ffffff";
 }
 
 cor.onchange = () =>{
@@ -23,9 +40,7 @@ for(x=0;x<tamanho;x++){//td
     criar();
 }
 
-if(tamanho==10){
-    obra()
-}
+obra();//quadro de xadrez
 
 function criar(){
     let linha = document.createElement('tr');
@@ -59,64 +74,19 @@ function limpar(){
 }
 
 function obra(){
-    document.getElementsByTagName('td')[7].style.background = '#000';
-    document.getElementsByTagName('td')[8].style.background = '#000';
-    document.getElementsByTagName('td')[9].style.background = '#000';
-    document.getElementsByTagName('td')[16].style.background = '#000';
-    document.getElementsByTagName('td')[17].style.background = '#000';
-    document.getElementsByTagName('td')[18].style.background = 'rgb(190,199,230)';
-    document.getElementsByTagName('td')[19].style.background = '#000';
-    document.getElementsByTagName('td')[21].style.background = '#000';
-    document.getElementsByTagName('td')[22].style.background = '#000';
-    document.getElementsByTagName('td')[23].style.background = '#000';
-    document.getElementsByTagName('td')[25].style.background = '#000';
-    document.getElementsByTagName('td')[26].style.background = '#000';
-    document.getElementsByTagName('td')[27].style.background = 'rgb(208,6,6)';
-    document.getElementsByTagName('td')[28].style.background = '#000';
-    document.getElementsByTagName('td')[29].style.background = '#000';
-    document.getElementsByTagName('td')[31].style.background = '#000';
-    document.getElementsByTagName('td')[32].style.background = 'rgb(91,236,255)';
-    document.getElementsByTagName('td')[33].style.background = '#000';
-    document.getElementsByTagName('td')[34].style.background = '#000';
-    document.getElementsByTagName('td')[35].style.background = '#000';
-    document.getElementsByTagName('td')[36].style.background = 'rgb(190,199,230)';
-    document.getElementsByTagName('td')[37].style.background = '#000';
-    document.getElementsByTagName('td')[38].style.background = '#000';
-    document.getElementsByTagName('td')[41].style.background = '#000';
-    document.getElementsByTagName('td')[42].style.background = '#000';
-    document.getElementsByTagName('td')[43].style.background = '#333';
-    document.getElementsByTagName('td')[44].style.background = '#000';
-    document.getElementsByTagName('td')[45].style.background = 'rgb(208,6,6)';
-    document.getElementsByTagName('td')[46].style.background = '#000';
-    document.getElementsByTagName('td')[47].style.background = '#000';
-    document.getElementsByTagName('td')[52].style.background = '#000';
-    document.getElementsByTagName('td')[53].style.background = '#333';
-    document.getElementsByTagName('td')[54].style.background = '#333';
-    document.getElementsByTagName('td')[55].style.background = '#000';
-    document.getElementsByTagName('td')[56].style.background = '#000';
-    document.getElementsByTagName('td')[61].style.background = '#000';
-    document.getElementsByTagName('td')[62].style.background = '#333';
-    document.getElementsByTagName('td')[63].style.background = 'rgb(91,236,255)';
-    document.getElementsByTagName('td')[64].style.background = '#333';
-    document.getElementsByTagName('td')[65].style.background = '#333';
-    document.getElementsByTagName('td')[66].style.background = '#000';
-    document.getElementsByTagName('td')[67].style.background = '#000';
-    document.getElementsByTagName('td')[70].style.background = '#000';
-    document.getElementsByTagName('td')[71].style.background = '#333';
-    document.getElementsByTagName('td')[72].style.background = '#333';
-    document.getElementsByTagName('td')[73].style.background = '#333';
-    document.getElementsByTagName('td')[74].style.background = '#000';
-    document.getElementsByTagName('td')[75].style.background = '#000';
-    document.getElementsByTagName('td')[76].style.background = 'rgb(91,236,255)';
-    document.getElementsByTagName('td')[77].style.background = '#000';
-    document.getElementsByTagName('td')[80].style.background = '#000';
-    document.getElementsByTagName('td')[81].style.background = 'rgb(91,236,255)';
-    document.getElementsByTagName('td')[82].style.background = '#333';
-    document.getElementsByTagName('td')[83].style.background = '#000';
-    document.getElementsByTagName('td')[85].style.background = '#000';
-    document.getElementsByTagName('td')[86].style.background = '#000';
-    document.getElementsByTagName('td')[87].style.background = '#000';
-    document.getElementsByTagName('td')[90].style.background = '#000';
-    document.getElementsByTagName('td')[91].style.background = '#000';
-    document.getElementsByTagName('td')[92].style.background = '#000';
+    let i=0;
+    let voltar= (tamanho*2)-1;
+    let pular=tamanho-2;
+    while(i<(tamanho*tamanho)){
+        document.getElementsByTagName('td')[i].style.background = "#fff";
+        if(i==pular){
+            i++;
+            pular+=(tamanho*2);
+        }
+        else if(i==voltar){
+            i--;
+            voltar += (tamanho*2);
+        }
+        i+=2;
+    }
 }
